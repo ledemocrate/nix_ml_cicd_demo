a2b981902691e74b2ab01b3d247258048a8d22c1
Bruno Rodrigues <bruno@brodrigues.co>
2023-09-18 15:55:05 +0200

    first commit

---
+++ default.nix
@@ -0,0 +1,15 @@
+let
+ pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz") {};
+ rpkgs = builtins.attrValues {
+  inherit (pkgs.rPackages) tidymodels vetiver targets xgboost;
+};
+ system_packages = builtins.attrValues {
+  inherit (pkgs) R;
+};
+  in
+  pkgs.mkShell {
+    buildInputs = [  rpkgs system_packages  ];
+      shellHook = ''
+
+'';
+  }
